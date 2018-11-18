d3823bf025a5ddf232e6bd027d927478a2725c9c
Unknown <acc.admnv@gmail.com>
2018-11-19 00:43:55 +0100

    Design 5
    book list image size + cart update

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -11,14 +11,22 @@ export class CartComponent implements OnInit {
 
   cartKeys;
   cartList;
+  total;
 
   constructor(private cs: CartService, private os: OrderService) { }
 
   ngOnInit() {
+    this.total = 0;
     this.cartList = this.cs.getCart(false);
     if (this.cartList) {
       this.cartKeys = Object.keys(this.cartList);               
     }
+    console.log(this.cartList);
+    for(let k of this.cartKeys){
+      this.total += this.cartList[k].book.price * this.cartList[k].quantity;
+    }
+
+    console.log(this.total);
   }
 
   onClick() {
